e8c9f307ddb0204cd7f6b16761fa8a28f27dd549
RSamaium <sam@newbot.io>
2024-06-05 11:32:42 +0200

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './features/navbar/navbar.component';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    template: `
+        <app-navbar />
+        <h1>Mon App</h1>
+    `,
+    standalone: true,
+    imports: [NavbarComponent]
 })
-export class AppComponent {
-  title = 'myapp';
-}
+export class AppComponent {}
